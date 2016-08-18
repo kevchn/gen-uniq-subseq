0b4bbc1b33f73aaddc4a0d0db7eeedee1df4390d
Kevin Chen <kevchen98@gmail.com>
2016-08-18 13:36:24 -0400

    Process all mirna files regardless of filter

--- compute.py
+++ compute.py
@@ -9,17 +9,17 @@ def comp(a, b, c):
     motif_set = set()
     handle = open("data/sorted.human.mature.fa", "rU")
     for record in SeqIO.parse(handle, "fasta"):
+        index_5p = a-1
+        flag_motif_unique = False
+        motif = ""
+        while(flag_motif_unique == False):
+            motif = record.seq.back_transcribe()[index_5p:index_5p+motif_size]
+            if motif not in motif_set:
+                flag_motif_unique = True
+                motif_set.add(motif)
+            index_5p += 1
+        pos = "pos:" + str(index_5p) + "-" + str(index_5p+10)
         if record.id in mirna_set: # check if db mirna is one in user input
-            index_5p = a-1
-            flag_motif_unique = False
-            motif = ""
-            while(flag_motif_unique == False):
-                motif = record.seq.back_transcribe()[index_5p:index_5p+motif_size]
-                if motif not in motif_set:
-                    flag_motif_unique = True
-                    motif_set.add(motif)
-                index_5p += 1
-            pos = "pos:" + str(index_5p) + "-" + str(index_5p+10)
             results.append(">" + record.id + " " + motif + " " + pos)
             results.append(record.seq.back_transcribe())
         else:
